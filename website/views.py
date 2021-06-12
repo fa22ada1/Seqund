7ed9dc71bfe4532c7c680ded4f920754b0b5418d
OussamaAI <sakatoussama1@gmail.com>
2021-06-12 19:43:57 +0100

    fixed ovewritten endpoint error

--- website/views.py
+++ website/views.py
@@ -1,17 +1,18 @@
 from typing import KeysView
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify
+from flask_login import login_required, current_user
+from . import db
 import os
+import json
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
-def home():
-    return "<h1>Test</h1>"
-
+@login_required
 @views.route('/upload/')
 def home():
     return render_template('home.html')
 
+@login_required
 @views.route('/upload/', methods=['POST'])
 def upload_file():
     uploaded_file = request.files['file']
