f141d712bab424c836e0adf45f77f3352072c4d6
OussamaAI <sakatoussama1@gmail.com>
2021-06-14 21:57:17 +0100

    stylings

--- website/__init__.py
+++ website/__init__.py
@@ -8,7 +8,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config ['SECRET_KEY'] = 'sdvnweiniwRJ2343234'
+    app.config['SECRET_KEY'] = 'sdvnweiniwRJ2343234'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024 * 10  # accept files with size <10Gb
     app.config['UPLOAD_EXTENSIONS'] = ['.sff']                  # accept only .sff files
