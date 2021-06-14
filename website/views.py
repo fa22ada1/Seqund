680126e9f1702973a98e4c7e52e58582635a6f01
OussamaAI <sakatoussama1@gmail.com>
2021-06-14 17:29:37 +0100

    fastq extraction is functional

--- website/views.py
+++ website/views.py
@@ -1,12 +1,17 @@
-from typing import KeysView
-from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify, send_file
-from flask_login import login_required, current_user
-from . import db
-import os, subprocess,json
-
-from .sffextract import sfftofasta
+from flask import Blueprint, render_template, request, redirect, url_for, send_file
+from flask_login import current_user
+from .sffextract import sfftofasta, sfftofastq
+from wtforms import validators, RadioField
+from flask_wtf import Form
 
 views = Blueprint('views', __name__)
+
+class TestForm(Form):
+    choice_switcher = RadioField(
+        'Choice?',
+        [validators.Required()],
+        choices=[('choice1', 'Choice One'), ('choice2', 'Choice Two')], default='choice1'
+    )
 
 @views.route('/')
 def home():
@@ -18,17 +23,28 @@ def extract():
 
 @views.route('/extract/', methods=['POST'])
 def upload_file():
+    option = request.form.get('match')
+
+    print(option)
     uploaded_file = request.files['file']
     if uploaded_file.filename != '':
         ID = str(current_user.get_id())
         inputfl = "website/files/uploads/upload" + ID + ".sff"
-        outputfl = "website/files/outputs/output" + ID + ".fasta"
+        outputfl = "website/files/outputs/output" + ID
         uploaded_file.save(inputfl)
-        sfftofasta(inputfl,outputfl)
-    return redirect(url_for('views.downloadFile'))
+        if option:
+            outputfl = outputfl + '.fastq'
+            sfftofastq(inputfl,outputfl)
+            suf = 'fastq'
+        else :
+            outputfl = outputfl + '.fasta'
+            sfftofasta(inputfl,outputfl)
+            suf = 'fasta'
+        return redirect(url_for('views.downloadFile', flsuf = suf))
+    return render_template('extract.html', user=current_user)
 
-@views.route('download')
-def downloadFile():
+@views.route('download/<flsuf>')
+def downloadFile(flsuf):
     ID = str(current_user.get_id())
-    path = "files/outputs/output" + ID + ".fasta"
-    return send_file(path, as_attachment=True)
+    file = "files/outputs/output" + ID + '.' + flsuf
+    return send_file(file, as_attachment=True)
